56ce548cd2c198bb1fde9358fe7c4859307a9cb0
Olle Sandberg <olle@b3rg.se>
2013-10-08 14:15:26 +0000

    Keep text drawable inside the ScoreEffectObject class.
    git-svn-id: http://svn.gareth.se/swm/trunk@426 add37ce8-0dd3-4abe-8994-b1afb2f1f0e5

--- swm/src/se/gareth/swm/ScoreEffectObject.java
+++ swm/src/se/gareth/swm/ScoreEffectObject.java
@@ -2,32 +2,47 @@ package se.gareth.swm;
 
 import android.graphics.Canvas;
 
-public class ScoreEffectObject extends ActiveText {
+public class ScoreEffectObject extends ActiveObject {
 		
+    private final TextDrawable mText;
+
 	public ScoreEffectObject(GameBase gameBase,
 				double x, double y, double size, int color, int score) {
-		super(gameBase, x, y, size, "" + score);
+		super(gameBase);
         
-    	setColor(color);
+    	mText = new TextDrawable(gameBase.gameView.font, super.mPaint);
+    	mText.setTextSize(size, false);
+        mText.setColor(color);
+        mText.setTextAlign(TextDrawable.Align.CENTER);
 		mText.setOutline(game.res.getDimension(R.dimen.NormalFontOutline), 
 				game.res.getColor(R.color.NormalOutlineColor));
+        mText.setText("" + score);
+
         setPosition(x, y);
         setVelocity(Vector2D.DIRECTION_UP - 1.0, 20.0);
 		applyEffect(new FadeDestroyEffect(1.0));
+
+       	setDrawOrder(30);
 	}
 
 	public void changeScoreValue(int score) {
-		setText("" + score);
+		mText.setText("" + score);
 	}
 		 
 	@Override
-	public void update(double frameTime) {
-		super.update(frameTime);
+	public void setAlpha(int alpha) {
+		super.setAlpha(alpha);
+		mText.forceRedraw();
 	}
 	
+	@Override
+	public void update(double frameTime) {
+		super.update(frameTime);
+		mText.setPosition(getX(), getY());		
+	}
 	
 	@Override
 	public void draw(Canvas canvas) {
-		super.draw(canvas);
+		mText.draw(canvas);
 	}
 }
