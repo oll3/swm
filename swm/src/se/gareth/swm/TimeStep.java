8e262f67656c85dc8bf733135c706f4913f39a1f
Olle Sandberg <olle@b3rg.se>
2013-10-18 07:01:41 +0000

    Removed the average calculation for timestep, seems to have most negative impact after changing the movement of active objects.
    git-svn-id: http://svn.gareth.se/swm/trunk@445 add37ce8-0dd3-4abe-8994-b1afb2f1f0e5

--- swm/src/se/gareth/swm/TimeStep.java
+++ swm/src/se/gareth/swm/TimeStep.java
@@ -3,14 +3,12 @@ package se.gareth.swm;
 
 public class TimeStep {
 
-    private final AverageValue mTimeStepAverage;
 	private long mLastUpdate;
 	public long timeStepDiff;
 	public double timeStep;
 	public double timeStepSquared;
 
 	public TimeStep() {
-		mTimeStepAverage = new AverageValue(30);
 		reset();
 	}
 
@@ -21,9 +19,8 @@ public class TimeStep {
 	public void update() {
 		long ticks = System.currentTimeMillis();
 		timeStepDiff = ticks - mLastUpdate;
-		mTimeStepAverage.addValue(timeStepDiff);
 		mLastUpdate = ticks;
-		timeStep = mTimeStepAverage.getAvarageDouble()/1000.0;
+		timeStep = timeStepDiff / 1000.0;
 		timeStepSquared = timeStep * timeStep;
 	}
 
